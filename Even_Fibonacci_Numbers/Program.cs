a2a04e4a685b95a7952442567ee8f77d142c5d43
beginner721 <barankurtbogan@gmail.com>
2022-03-30 23:44:36 +0300

    Add project files.

---
+++ Even_Fibonacci_Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿List<int> numbers = new List<int>();
+
+numbers.Add(1);
+numbers.Add(2);
+
+while (numbers.Last() < 4000000)
+{
+    int last = numbers.Count();
+    numbers.Add(numbers[last-2]+numbers[last-1]);
+    
+}
+int sum = 0;
+foreach (var item in numbers)
+{
+    if (item%2==0 && item<4000000)
+    {
+        sum=sum+item;
+    }
+}
+Console.WriteLine(sum);
