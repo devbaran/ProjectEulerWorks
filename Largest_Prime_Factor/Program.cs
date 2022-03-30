5a1cda575435cdeef2f316e024a7e142bbc6d435
beginner721 <barankurtbogan@gmail.com>
2022-03-31 00:11:35 +0300

    Largest prime factor solving.

---
+++ Largest_Prime_Factor/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+*/
+
+long number = 600851475143;
+long dividing = 2;
+long maxPrime = 0;
+
+while (true)
+{
+    if (number% dividing != 0)
+    {
+        dividing = dividing + 1;
+    }
+    else
+    {
+        maxPrime = dividing;
+        number = number/ dividing;
+        if (number==1)
+        {
+            Console.WriteLine(maxPrime);
+            break;
+        }
+
+    }
+}
